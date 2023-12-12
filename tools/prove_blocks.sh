10743b91832fc051e510612f94ae3437301ef077
John Hilliard <praetoriansentry@gmail.com>
2023-12-12 09:10:49 -0500

    fix: upping the prover circuit sizes and making filenames sortable

--- tools/prove_blocks.sh
+++ tools/prove_blocks.sh
@@ -8,13 +8,13 @@
 export RUST_BACKTRACE=1
 export RUST_LOG=plonky2=trace,plonky2_evm=trace,proof_protocol_decoder=trace
 
-export ARTITHMETIC_CIRCUIT_SIZE="16..23"
-export BYTE_PACKING_CIRCUIT_SIZE="9..21"
-export CPU_CIRCUIT_SIZE="12..25"
-export KECCAK_CIRCUIT_SIZE="14..20"
-export KECCAK_SPONGE_CIRCUIT_SIZE="9..15"
-export LOGIC_CIRCUIT_SIZE="12..18"
-export MEMORY_CIRCUIT_SIZE="17..28"
+export ARTITHMETIC_CIRCUIT_SIZE="16..28"
+export BYTE_PACKING_CIRCUIT_SIZE="9..28"
+export CPU_CIRCUIT_SIZE="12..28"
+export KECCAK_CIRCUIT_SIZE="14..28"
+export KECCAK_SPONGE_CIRCUIT_SIZE="9..28"
+export LOGIC_CIRCUIT_SIZE="12..28"
+export MEMORY_CIRCUIT_SIZE="17..30"
 
 PROOF_OUTPUT_DIR="proofs"
 ALWAYS_WRITE_LOGS=0 # Change this to `1` if you always want logs to be written.
@@ -28,28 +28,35 @@ for ((i=$1; i<=$2; i++))
 do
     echo "Proving block ${i}..."
 
-    OUT_PROOF_PATH="${PROOF_OUTPUT_DIR}/b${i}.zkproof"
-    OUT_LOG_PATH="${PROOF_OUTPUT_DIR}/b${i}.log"
+    out_idx=$(printf "%05d" $i)
 
-    if [ $i -gt 1 ]; then
+    OUT_PROOF_PATH="${PROOF_OUTPUT_DIR}/b${out_idx}.zkproof"
+    OUT_LOG_PATH="${PROOF_OUTPUT_DIR}/b${out_idx}.log.bz2"
+
+    if [[ $i -gt 1 ]]; then
         prev_proof_num=$((i-1))
-        PREV_PROOF_EXTRA_ARG="-f ${PROOF_OUTPUT_DIR}/b${prev_proof_num}.zkproof"
+	prev_proof_num_idx=$(printf "%05d" $prev_proof_num)
+        PREV_PROOF_EXTRA_ARG="-f ${PROOF_OUTPUT_DIR}/b${prev_proof_num_idx}.zkproof"
     fi
 
     # echo "cheese"
     # echo "cargo r --release --bin leader -- --runtime in-memory jerigon --rpc-url "$3" --block-number $i --proof-output-path $OUT_PROOF_PATH $PREV_PROOF_EXTRA_ARG > $OUT_LOG_PATH 2>&1"
-    cargo r --release --bin leader -- --runtime in-memory jerigon --rpc-url "$3" --block-number $i --proof-output-path $OUT_PROOF_PATH $PREV_PROOF_EXTRA_ARG > $OUT_LOG_PATH 2>&1
-    
-    retVal=$?
-    if [ $retVal -ne 0 ]; then
-        # Some error occured.
-        echo "Block ${i} errored. See ${OUT_LOG_PATH} for more details."
-        exit $retVal
-    else
-        # Remove the log on success if we don't want to keep it.
-        if [ $ALWAYS_WRITE_LOGS -ne 0 ]; then
+    cargo r --release --bin leader -- \
+	  --runtime in-memory jerigon \
+	  --rpc-url "$3" \
+	  --block-number $i \
+	  --proof-output-path $OUT_PROOF_PATH \
+	  $PREV_PROOF_EXTRA_ARG 2>&1 | bzip2 > $OUT_LOG_PATH
+    # touch $OUT_LOG_PATH
+    # touch $OUT_PROOF_PATH
+
+    if [[ -e $OUT_PROOF_PATH ]]; then
+        if [[ $ALWAYS_WRITE_LOGS -ne 0 ]]; then
             rm $OUT_LOG_PATH
         fi
+    else
+	echo "Block ${i} errored. See ${OUT_LOG_PATH} for more details."
+	exit 1
     fi
 done
 
